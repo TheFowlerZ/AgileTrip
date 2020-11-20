795010ff0b5c57ea88f4e567ad7d6f9d05eeb243
Thibault Libert <thibault.libert@he-arc.ch>
2020-11-20 15:45:44 +0100

    Install bootstrap vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,20 +15,20 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
-      },
-    ],
-    script: [
-      {
-        src:
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
-        integrity:
-          'sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV',
-        crossorigin: 'anonymous',
-      },
+      //   {
+      //     rel: 'stylesheet',
+      //     href:
+      //       'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+      //   },
+      // ],
+      // script: [
+      //   {
+      //     src:
+      //       'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
+      //     integrity:
+      //       'sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV',
+      //     crossorigin: 'anonymous',
+      //   },
     ],
   },
 
@@ -51,6 +51,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'bootstrap-vue/nuxt',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
