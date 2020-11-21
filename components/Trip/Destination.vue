002f952bf1af4f66f966b1760fb0328346f3566a
Thibault Libert <thibault.libert@he-arc.ch>
2020-11-21 14:31:38 +0100

    AT-30 AT-28 Implement add destination

---
+++ components/Trip/Destination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="destination-box mb-3 p-2">
+    <b-form inline>
+      <b-form-group class="m-2" label="Destination">
+        <b-form-input
+          v-model="destination"
+          placeholder="Saisissez une destination"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group class="m-2" label="Date de début">
+        <b-form-datepicker
+          id="date-debut"
+          v-model="dateDebut"
+        ></b-form-datepicker>
+      </b-form-group>
+      <b-form-group class="m-2" label="Date de fin" :col="12">
+        <b-form-datepicker id="date-fin" v-model="dateFin"></b-form-datepicker>
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    destination: '',
+    dateDebut: '',
+    dateFin: '',
+  }),
+}
+</script>
+
+<style>
+.destination-box {
+  border: 1px solid gray;
+  border-radius: 5px;
+}
+</style>
---
+++ pages/create-trip/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Créer un voyage personnalisé</h3>
+    <Destination
+      v-for="destination in destinations"
+      :key="destination.id"
+    ></Destination>
+    <b-button @click="addDestination">+ Ajouter une destination</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    count: 0,
+    destinations: [],
+  }),
+  methods: {
+    addDestination() {
+      this.destinations.push({
+        id: this.count++,
+      })
+    },
+  },
+}
+</script>
+
+<style></style>
